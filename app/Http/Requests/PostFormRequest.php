deecb38420667334f1c679d726aea13ef6f4d128
s.oguchi <zhishixiaokou.private@gmail.com>
2022-10-26 12:09:40 +0000

    Postモデルを追加、posts作成時のFormRequestを追加

---
+++ app/Http/Requests/PostFormRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PostFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required|max:255',
+            'content' => 'required|max:255',
+            'input_file' => 'required|file|image|mimes:jpeg,png,jpg,gif|max:1024'
+        ];
+    }
+}
---
+++ app/Post.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Post extends Model
+{
+    protected $fillable = [
+        'title',
+        'content',
+        'file_name',
+        'user_id',
+        'file_path'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+}
