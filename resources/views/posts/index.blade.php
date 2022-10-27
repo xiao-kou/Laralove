1218c76a18f8956cc24778f84a1a6d1c597f2862
s.oguchi <zhishixiaokou.private@gmail.com>
2022-10-27 15:08:32 +0000

    post一覧のレイアウトを修正

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,9 +6,9 @@
 
 @include('nav')
 <div class="container">
-    <div class="row d-flex mt-1">
+    <div class="row d-flex align-items-center mt-1">
         @foreach($posts as $post)
-            <div class="col-4">
+            <div class="col-md-8 offset-md-2 card my-3 bg-light">
                 <a href="{{ route('post.show', ['id' => $post->id]) }}"><img src="{{ asset($post->file_path) }}" class="w-100" alt="..."></a>
             </div>
         @endforeach
