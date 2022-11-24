0bfc105697b8318654e3aa0bf1a0870cc1f00906
s.oguchi <zhishixiaokou.private@gmail.com>
2022-11-24 04:18:57 +0000

    ダイレクトメッセージ一覧画面のモックアップを追加

--- resources/views/rooms/index.blade.php
+++ resources/views/rooms/index.blade.php
@@ -6,4 +6,17 @@
 
 @include('nav')
 
+<div class="container">
+    <div class="d-flex justify-content-start align-items-center mt-3">
+        <a href=""><img src="" alt="" class="rounded-circle circle-sm"></a>
+        <div class="ml-4">
+            <div class="d-flex align-items-center">
+                <h5>ユーザー名</h5>
+                <h6 class="ml-3 mb-2 text-muted">スクリーンID</h6>
+            </div>
+            <div>メッセージの内容</div>
+        </div>
+    </div>
+    <hr>
+</div>
 @endsection
