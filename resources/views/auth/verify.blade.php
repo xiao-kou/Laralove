4c1a3a7c460e0af22d18492da286ba1e8e948b8d
s.oguchi <zhishixiaokou.private@gmail.com>
2022-12-02 09:26:04 +0000

    メールアドレス確認テンプレートにサイドバーを追加

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -6,27 +6,32 @@
 
 @include('nav')
 
-<div class="container">
-    <div class="row justify-content-center mt-3">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('メールアドレスを確認してください。') }}</div>
+<div class="container-fluid pl-0">
+    <div class="row">
+        @include('sidebar')
+        <div class="col">
+            <div class="row justify-content-center mt-3">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">{{ __('メールアドレスを確認してください。') }}</div>
 
-                <div class="card-body">
-                    @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('新しい確認リンクがメールアドレスに送信されました。') }}
+                        <div class="card-body">
+                            @if (session('resent'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ __('新しい確認リンクがメールアドレスに送信されました。') }}
+                                </div>
+                            @endif
+
+                            <div>
+                                {{ __('メールに送信されたリンクから本登録を行なってください。') }}
+                            </div>
+                            {{ __('メールが届かない場合は、') }}
+                            <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
+                                @csrf
+                                <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('ここをクリックして確認リンクを再送信する。') }}</button>
+                            </form>
                         </div>
-                    @endif
-
-                    <div>
-                        {{ __('メールに送信されたリンクから本登録を行なってください。') }}
                     </div>
-                    {{ __('メールが届かない場合は、') }}
-                    <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
-                        @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('ここをクリックして確認リンクを再送信する。') }}</button>
-                    </form>
                 </div>
             </div>
         </div>
