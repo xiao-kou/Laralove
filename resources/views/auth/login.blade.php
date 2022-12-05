08ce44505ad49e8adf4a6a1a6cfe07ff4f512870
s.oguchi <zhishixiaokou.private@gmail.com>
2022-12-05 11:38:07 +0000

    テンプレートのオートフォーカスを削除

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,7 +23,7 @@
                                     <label for="email" class="col-md-4 col-form-label text-md-right">メールアドレス</label>
 
                                     <div class="col-md-6">
-                                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
                                         <span class="invalid-feedback email" role="alert">
                                             <strong>メールアドレスの形式が無効です。</strong>
